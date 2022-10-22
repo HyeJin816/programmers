61bbd84bf4b799e00a3f0d746b52eab7e3d2bfe1
hye.jin <90254354+HyeJin816@users.noreply.github.com>
2022-10-22 12:34:18 +0900

    Update 221021_가장 큰 정사각형.py

--- python_Lv.2/221021_가장 큰 정사각형.py
+++ python_Lv.2/221021_가장 큰 정사각형.py
@@ -1,29 +1,15 @@
-import numpy as np
-
 def solution(board):
-    clear_x = change(board, MAXnum(board))
-    
-    if MAXnum(clear_x) == 1:
-        return 1
-    elif MAXnum(clear_x) == 0:
-        return 0
-    
-    new = (np.array(clear_x)).transpose()
-    new = new.tolist()
-    clear_y = change(new, MAXnum(new))
-    
-    return MAXnum(clear_y)**2
-    
-### 최대 정사각형이 들어갈 수 없으면 전체 0으로 변환    
-def change(board,answer):
-    for line in range(len(board)):
-        if sum(board[line]) < answer:
-            board[line] = [0] * len(board[line])
-    return board
+    count = 0
+    lb = len(board)
+    lr = len(board[0])
 
-### 전체 합으로 최대 정사각형 크기 반환
-def MAXnum(board):
-    num = 0
-    for x in board:
-        num += sum(x)
-    return int(num**(1/2))
+    if (lb<2) or (lr<2):
+        return 1
+
+    for r in range(1,lb):
+        for c in range(1,lr):
+            if board[r][c] != 0:
+                board[r][c] = min(board[r][c-1], board[r-1][c], board[r-1][c-1])+1
+                count = max(count, board[r][c])
+                
+    return count**2
